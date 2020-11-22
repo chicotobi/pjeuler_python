a005902ce76ceda5c3347a43e23458cb7e0d36d5
chicotobi <tobias310788@googlemail.com>
2020-11-22 14:42:35 +0100

    simplify 54

--- in_progress/54.py
+++ in_progress/54.py
@@ -1,68 +1,52 @@
 from collections import Counter
 import math
 
-value = 0
-
-
 def is_royal_flush(ranks, suits):
-    global value
-    if is_straight(ranks) and is_flush(ranks, suits) and ranks[0] == 10:
-        value = 10e6
-        return True
-    return 0
-
+    if is_straight(ranks)[0] and is_flush(ranks, suits)[0] and ranks[0] == 10:
+        return True, 10e6
+    return False, 0
 
 def is_straight_flush(ranks, suits):
-    global value
-    if is_straight(ranks) and is_flush(ranks, suits):
-        value = 9e6
-        return True
-    return 0
+    if is_straight(ranks)[0] and is_flush(ranks, suits)[0]:
+        return True, 9e6
+    return False, 0
 
 
 def is_four(ranks):
-    global value
     x = Counter(ranks).most_common(2)
     if x[0][1] == 4:
-        value = 8e6
-        return True
-    return False
+        return True, 8e6
+    return False, 0
 
 
 def is_full_house(ranks):
-    global value
     x = Counter(ranks).most_common(2)
     if x[0][1] == 3 and x[1][1] == 2:
-        value = 7e6 + x[0][0]*15 + x[1][0]
-        return True
-    return False
+        return True, 7e6 + x[0][0]*15 + x[1][0]
+    return False, 0
 
 
 def is_flush(ranks, suits):
-    global value
     if suits[0] == suits[1] and suits[0] == suits[2] and suits[0] == suits[3] and suits[0] == suits[4]:
         ranks.sort()
-        value = 6e6 + ranks[4] * 15 ** 4 + ranks[3] * 15 ** 3 + ranks[2] * 15 ** 2 + ranks[1] * 15 * ranks[0]
-        return True
-    return False
+        return True, 6e6 + ranks[4] * 15 ** 4 + ranks[3] * 15 ** 3 + ranks[2] * 15 ** 2 + ranks[1] * 15 * ranks[0]
+    return False, 0
 
 
 def is_straight(ranks):
-    global value
     x = Counter(ranks).most_common(5)
     if x[0][1]==1 and ranks[4] - ranks[0] == 4:
-        value = 5e6 + ranks[0]
-        return True
+        return True, 5e6 + ranks[0]
+    return False, 0
 
 
 def is_three(ranks):
-    global value
     x = Counter(ranks).most_common(5)
     if x[0][1] == 3:
         tmp = [x[1][0], x[2][0]]
         tmp.sort()
-        value = 4e6 + x[0][0] * 15 ** 2 + tmp[1] * 15 + tmp[0]
-        return True
+        return True, 4e6 + x[0][0] * 15 ** 2 + tmp[1] * 15 + tmp[0]
+    return False, 0
 
 
 def is_two_pair(ranks):
@@ -71,8 +55,8 @@ def is_two_pair(ranks):
     if x[0][1] == 2 and x[1][1] == 2:
         tmp = [x[0][0], x[1][0]]
         tmp.sort()
-        value = 3e6 + tmp[1] * 15 ** 2 + tmp[0] * 15 + x[2][0]
-        return True
+        return True, 3e6 + tmp[1] * 15 ** 2 + tmp[0] * 15 + x[2][0]
+    return False, 0
 
 
 def is_one_pair(ranks):
@@ -81,16 +65,16 @@ def is_one_pair(ranks):
     if x[0][1] == 2:
         tmp = [x[1][0], x[2][0], x[3][0]]
         tmp.sort()
-        value = 2e6 + x[0][0] * 15 ** 3 + tmp[2] * 15**2 + tmp[1] * 15 + tmp[0]
-        return True
+        return True, 2e6 + x[0][0] * 15 ** 3 + tmp[2] * 15**2 + tmp[1] * 15 + tmp[0]
+    return False, 0
 
 
 def is_high_card(ranks):
     global value
     x = Counter(ranks).most_common(5)
     if x[0][1] == 1:
-        value = 1e6 + ranks[4] * 15 ** 4 + ranks[3] * 15 ** 3 + ranks[2] * 15 ** 2 + ranks[1] * 15 + ranks[0]
-        return True
+        return True, 1e6 + ranks[4] * 15 ** 4 + ranks[3] * 15 ** 3 + ranks[2] * 15 ** 2 + ranks[1] * 15 + ranks[0]
+    return False, 0
 
 
 def hand_to_rankssuits(hand):
@@ -104,33 +88,30 @@ def hand_to_rankssuits(hand):
 def eval_hand(hand):
     ranks, suits = hand_to_rankssuits(hand)
     ranks.sort()
-    if is_royal_flush(ranks, suits):
-        1 + 1
-    elif is_straight_flush(ranks, suits):
-        1 + 1
-    elif is_four(ranks):
-        1 + 1
-    elif is_full_house(ranks):
-        1 + 1
-    elif is_flush(ranks, suits):
-        1 + 1
-    elif is_straight(ranks):
-        1 + 1
-    elif is_three(ranks):
-        1 + 1
-    elif is_two_pair(ranks):
-        1 + 1
-    elif is_one_pair(ranks):
-        1 + 1
-    elif is_high_card(ranks):
-        1 + 1
-    else:
-        print('what?')
-        quit()
+    [flag, value] = is_royal_flush(ranks, suits)
+    if not flag:
+      [flag, value] = is_straight_flush(ranks, suits)
+    if not flag:
+      [flag, value] = is_four(ranks)
+    if not flag:
+      [flag, value] = is_full_house(ranks)
+    if not flag:
+      [flag, value] = is_flush(ranks, suits)
+    if not flag:
+      [flag, value] = is_straight(ranks)
+    if not flag:
+      [flag, value] = is_three(ranks)
+    if not flag:
+      [flag, value] = is_two_pair(ranks)
+    if not flag:
+      [flag, value] = is_one_pair(ranks)
+    if not flag:
+      [flag, value] = is_high_card(ranks)
     return value
 
 
-f = open('p054_poker.txt', 'r')
+f = open('/home/hofmann/pjeuler_python/pjeuler/input/54')
+
 wins1 = 0
 counter = 0
 for both_hands in f.readlines():
