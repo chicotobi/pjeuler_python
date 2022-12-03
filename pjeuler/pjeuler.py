d635b227573000cd102c163164e1a1add1c294f3
chicotobi <tobias310788@googlemail.com>
2022-12-03 23:42:32 +0100

    108 110

--- pjeuler/pjeuler.py
+++ pjeuler/pjeuler.py
@@ -4,6 +4,7 @@ import itertools
 import collections
 import numpy as np
 import pandas as pd
+import sympy
 
 def pjeuler(i):
   return eval("pjeuler"+str(i)+"()")
@@ -1762,6 +1763,34 @@ def pjeuler107():
   res = scipy.sparse.csgraph.minimum_spanning_tree(a)
   return int(.5 * np.sum(a)-np.sum(res))
 
+def pjeuler108():
+  def n_from_lst(lst):
+    primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+    return np.prod([float(b)**e for (b,e) in zip(primes,lst)])
+    
+  def sol_from_lst(lst):
+    ans = np.prod([2*x+1 for x in lst])
+    return (ans+1)//2  
+  
+  @functools.lru_cache(None)
+  def partition(number,maxlen):
+    answer = [[number]]
+    for x in range(1, number):
+      for y in partition(number - x,maxlen-1):
+        if len(y) <= maxlen - 1 and x >= max(y):
+          answer.append([x]+y)
+    return answer
+  
+  val = 1000
+  n = 1e10
+  max_len = 10
+  max_s = 10
+  for s in range(1,max_s+1):
+    for p in partition(s,max_len):
+      if sol_from_lst(p) > val:
+        n = min(n,n_from_lst(p))
+  return(int(n))
+  
 def pjeuler109():
   fields = []
   for m in range(1,4):
@@ -1794,6 +1823,34 @@ def pjeuler109():
                 sols += 1
                 continue
   return sols
+
+def pjeuler110():
+  def n_from_lst(lst):
+    primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47]
+    return np.prod([float(b)**e for (b,e) in zip(primes,lst)])
+    
+  def sol_from_lst(lst):
+    ans = np.prod([2*x+1 for x in lst])
+    return (ans+1)//2  
+  
+  @functools.lru_cache(None)
+  def partition(number,maxlen):
+    answer = [[number]]
+    for x in range(1, number):
+      for y in partition(number - x,maxlen-1):
+        if len(y) <= maxlen - 1 and x >= max(y):
+          answer.append([x]+y)
+    return answer
+  
+  val = 4000000
+  n = 1e20
+  max_len = 15
+  max_s = 20
+  for s in range(1,max_s+1):
+    for p in partition(s,max_len):
+      if sol_from_lst(p) > val:
+        n = min(n,n_from_lst(p))
+  return(int(n))
 
 def pjeuler112():
   def d(x):
