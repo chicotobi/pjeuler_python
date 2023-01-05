4a4d49af254b7dfde5b73804728bb591f274c497
Tobias Hofmann (rm2mw061244) <tobias.hofmann.th1@roche.com>
2023-01-05 16:48:38 +0100

    103 105 106

--- pjeuler/pjeuler.py
+++ pjeuler/pjeuler.py
@@ -1731,6 +1731,62 @@ def pjeuler102():
   f.close()
   return n
 
+def pjeuler103():
+  from itertools import chain, combinations
+  from functools import cache
+
+  def powerset(iterable):
+      s = list(iterable)
+      return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+  @cache
+  def is_special(t):
+    if len(t) < 3:
+      return True
+
+    # Check for disjoints subsets B and C with a maximum of n-1 elements
+    for idx in range(len(t)-1,-1,-1):
+      t1 = t[:idx] + t[idx+1:]
+      if not is_special(t1):
+        return False
+
+    # Check for subsets B and C with exactly n elements
+    # That means, A = B + C and B intersect C = {}
+    for t1 in powerset(t):
+      if len(t1)==0 or len(t1)==len(t):
+        continue
+      s0 = set(t)
+      s1 = set(t1)
+      s2 = s0.difference(s1)
+      if len(s1) > len(s2) and sum(s1)<=sum(s2):
+        return False
+      if sum(s1) == sum(s2):
+        return False
+    return True
+
+  @cache
+  def all_special_sets(suma,n):
+    s = set()
+    if n==1:
+      t = tuple([suma])
+      s.add(t)
+      return s
+    for x in range(suma):
+      for t0 in all_special_sets(suma-x, n-1):
+        if x not in t0:
+          t1 = tuple(sorted(t0+tuple([x])))
+          if is_special(t1):
+            s.add(t1)
+    return s
+
+  suma = 0
+  while True:
+    res = all_special_sets(suma, 7)
+    if len(res) > 0:
+      break
+    suma += 1
+  return int(''.join(str(i) for i in list(res)[0]))
+
 def pjeuler104():
   a = 1
   b = 1
@@ -1747,6 +1803,91 @@ def pjeuler104():
       if begin:
         break
   return k
+
+def pjeuler105():
+  from itertools import chain, combinations
+  from functools import cache
+
+  def powerset(iterable):
+      s = list(iterable)
+      return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+  @cache
+  def is_special(t):
+    if len(t) < 3:
+      return True
+
+    # Check for disjoints subsets B and C with a maximum of n-1 elements
+    for idx in range(len(t)-1,-1,-1):
+      t1 = t[:idx] + t[idx+1:]
+      if not is_special(t1):
+        return False
+
+    # Check for subsets B and C with exactly n elements
+    # That means, A = B + C and B intersect C = {}
+    for t1 in powerset(t):
+      if len(t1)==0 or len(t1)==len(t):
+        continue
+      s0 = set(t)
+      s1 = set(t1)
+      s2 = s0.difference(s1)
+      if len(s1) > len(s2) and sum(s1)<=sum(s2):
+        return False
+      if sum(s1) == sum(s2):
+        return False
+    return True
+
+  d = [i.split(',') for i in open('../pjeuler/input/105').read().splitlines()]
+  d = [tuple(int(k) for k in i) for i in d]
+
+  s = 0
+  for i in d:
+    if is_special(i):
+      s += sum(i)
+  return s
+
+def pjeuler106():
+  from itertools import chain, combinations, permutations
+
+  def sum_is_certainly_not_equal(t1,t2):
+    assert(len(t1)==len(t2))
+    assert(len(t1)>1)
+    for p in permutations(range(len(t1))):
+      v = [t1[i] < t2[j] for (i,j) in enumerate(p)]
+      if all(v):
+        return True
+    return False
+
+  def powerset(iterable):
+      s = list(iterable)
+      return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+  x = list(range(12))
+
+  s = set()
+  for t1 in powerset(x):
+    if len(t1)==0 or len(t1)==len(x):
+      continue
+    x2 = [i for i in x if i not in t1]
+    for t2 in powerset(x2):
+      if len(t2)==0:
+        continue
+      if len(t2)>len(t1):
+        continue
+      if (t1,t2) not in s and (t2,t1) not in s:
+        s.add((t1,t2))
+
+  n = 0
+  for i in s:
+    t1, t2 = i
+    if len(t1) != len(t2):
+      continue
+    if len(t1) == 1:
+      continue
+    if sum_is_certainly_not_equal(t1, t2):
+      continue
+    n += 1
+  return n
 
 def pjeuler107():
   import scipy.sparse.csgraph
--- test.py
+++ test.py
@@ -56,13 +56,18 @@ class TestProblems(unittest.TestCase):
 
   def test_rest(self):
     sols = {}
+
     sols[101] = 'd382b0cc25e82446da83d3a792e1cd27'
     sols[102] = '74db120f0a8e5646ef5a30154e9f6deb'
+    sols[103] = 'af8c238336c2a79bb81a24b3fef3330d'
     sols[104] = 'c8771ddd4df191098d70a8e94dd1cde7'
+    sols[105] = 'c87d30e494eff438fe37b4c810167da0'
+    sols[106] = 'c8fd9e36fdeb06bcc93a0732c667b6d8'
     sols[107] = 'b0db1202ec966e7855ca23626eb285b8'
     sols[108] = '765ba18edd2844db2db95fba25d2f3e7'
     sols[109] = 'e6aebd5be1ba81557dbcc5f6f57bbe5c'
     sols[110] = '591a7a92f10322866e6a02f3b2386a1c'
+
     sols[112] = 'e08c982713a1c2bd3637dd489199722e'
     sols[113] = 'a9e504ee704c87f9bddad6d3ffe39532'
     sols[114] = 'de48ca72bf252a8be7e0aad762eadcf8'
@@ -71,22 +76,30 @@ class TestProblems(unittest.TestCase):
     sols[117] = '542612809b3dd08cf518b85450fce8d6'
     sols[119] = '72fddfa6c52a120892ade628f3819da4'
     sols[120] = '0dd05ec40fe11279c2203b72e92a450a'
+
     sols[121] = '51de85ddd068f0bc787691d356176df9'
     sols[122] = 'b710915795b9e9c02cf10d6d2bdb688c'
     sols[123] = '71497f728b86b55d965edbf1849cca8d'
     sols[124] = 'f228d2e6f9099153388e9470180c8302'
     sols[125] = '1b5635e8ab723e01570ca783129493dd'
     sols[126] = '387d6ae83cbc6fa0b9192b56bf095c49'
+
     sols[145] = '705e8444ad9c92e9a7589fb97515a9b6'
+
     #sols[155] = 'da0a3fc900cc8ae42d514e280524ee39'
+
     sols[169] = 'd149d4836703a8908becea56ddd3ed42'
+
     sols[172] = 'f5f260ee21ead7478403c2ccd18a1829'
     sols[173] = '177f825c89a68aefae37b8dec9bb8a9b'
     #sols[177] = 'd7a85236af930db0f7e84f2de8ee7ac2'
     sols[179] = 'bafa0132bc7fc422a8d53bebb9d003c9'
+
     sols[187] = 'b3e6977523511d2cbbef8fccb1e394db'
+
     sols[191] = 'e04dfa598b22a87570f63063f3ff595d'
     sols[199] = '0f8fd87159c28ae5fea6ac91a95d48dd'
+
 
     sols[203] = 'd7ec16d216c923d3c927f46cfc914e92'
     sols[205] = 'ba6c6c3888227a0799eca38191b587be'
